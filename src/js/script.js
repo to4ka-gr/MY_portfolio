958c5de64397c1b4f74dfd1c843fa63512f4f6a9
Dmitrii <to4ka180294@gmail.com>
2023-03-24 14:16:35 +0600

    add forth screen and percent script

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu');
+      menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
@@ -8,4 +8,11 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+const percent = document.querySelectorAll('.skills__lvl-percent'),
+      lines = document.querySelectorAll('.skills__lvl-scale div');
+
+percent.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
