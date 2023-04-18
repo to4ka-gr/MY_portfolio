7aa9d0a4d8fda4149a2ae634c736375962575c68
Dmitrii <to4ka180294@gmail.com>
2023-04-18 13:15:22 +0600

    preview

---
+++ dist/js/scriptPulse.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/icons/carousel/chevron_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/icons/carousel/chevron_right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide (item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+
+    toggleSlide ('.catalog-item__link');
+    toggleSlide ('.catalog-item__back');
+
+    // modals
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        });
+    });
+
+    // validation
+
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Введите свое имя",
+                    minlength: jQuery.validator.format("Введите минимально {0} символа!")
+                  },
+                phone: "Введите свой номер телефона",
+                email: {
+                  required: "Введите свою почту",
+                  email: "Введите корректный адрес"
+                }
+            }
+        });
+    }
+
+    valideForms('#consultation-form');
+    valideForms(' #consultation form');
+    valideForms('#order form');
+
+    $('input[name=phone]').mask("+996(999)99-99-99");
+
+    // ajax forms without reload
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('forms').trigger('reset');
+        });
+        return false;
+    });
+
+    // pageup img smooth sxroll
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    // animate wow instal
+    new WOW().init();
+});
--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,53 @@ const percent = document.querySelectorAll('.skills__lvl-percent'),
 percent.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+// jqValid
+
+/* $("#myform").validate({
+    rules: {
+        name: "required",
+        email: {
+            required: true,
+            email: true
+        },
+        text: "required",
+
+    },
+    messages: {
+        name: {
+            required: "Введите свое имя",
+            minlength: jQuery.validator.format("Введите минимально {0} символа!")
+          },
+        text: {
+            minlength: 20,
+            maxlenght: 120
+        },
+        email: {
+          required: "Введите свою почту",
+          email: "Введите корректный адрес"
+        }
+    }
+}); */
+
+
+// phpMailer
+
+/* $('form').submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('forms').trigger('reset');
+    });
+    return false;
+}); */
